2794245e8b7896c20319d2c9bf910ede6850b67f
viktorak6gu <124782787+viktorak6gu@users.noreply.github.com>
2024-04-18 10:40:17 +0200

    Add files via upload

---
+++ ITSM/Controllers/CommentController.cs
@@ -0,0 +1,39 @@
+﻿using ITSM.Services;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ITSM.Controllers
+{
+    [Authorize]
+    public class CommentController : Controller
+    {
+        private readonly ICommenctService _commentService;
+
+        public CommentController(ICommenctService commentService)
+        {
+            _commentService = commentService;
+        }
+
+        [HttpPost]
+        public IActionResult AddComment(int projectId, string message)
+        {
+            var result = _commentService.AddComment(projectId, message, User);
+
+            if(result) TempData["success"] = "Comment has been added.";
+            else TempData["error"] = "Something went wrong while creating your comment.";
+
+            return RedirectToAction("Details", "Project", new { id = projectId });
+        }
+
+        [HttpPost]
+        public IActionResult DeleteComment(int projectId, int commentId)
+        {
+            var result = _commentService.DeleteComment(commentId, User);
+
+            if(result) TempData["success"] = "Comment has been removed.";
+            else TempData["error"] = "Something went wrong while creating your comment.";
+
+            return RedirectToAction("Details", "Project", new { id = projectId });
+        }
+    }
+}
---
+++ ITSM/Controllers/ProjectController.cs
@@ -0,0 +1,78 @@
+﻿using ITSM.Data;
+using ITSM.Models;
+using ITSM.Services;
+using ITSM.ViewModels;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace ITSM.Controllers
+{
+    [Authorize]
+    public class ProjectController : Controller
+    {
+        private readonly IProjectService _projectService;
+
+        public ProjectController(IProjectService projectService)
+        {
+            _projectService = projectService;
+        }
+
+        [HttpGet]
+        public IActionResult Index()
+        {
+            var truncatedProjects = _projectService.GetAll();
+
+            return View(truncatedProjects);
+        }
+
+        [HttpPost]
+        public IActionResult Create(ProjectDetailsViewModel projectDetailsVM)
+        {
+            if (!ModelState.IsValid)
+                return View(nameof(Details), projectDetailsVM);
+
+            var result = _projectService.Create(projectDetailsVM);
+
+            if (result) TempData["success"] = "Project has been created successfully.";
+            else TempData["error"] = "Something went wrong while creating a project.";
+
+            return RedirectToAction(nameof(Index));
+        }
+
+        [HttpGet]
+        public IActionResult Details(int id)
+        {
+            var projectDetailsVM = _projectService.GetProjectDetailsVM(id);
+
+            if (projectDetailsVM == null)
+                return RedirectToAction(nameof(Index));
+
+            return View(projectDetailsVM);
+        }
+
+        [HttpPost]
+        public IActionResult Update(ProjectDetailsViewModel projectDetailsVM, int id)
+        {
+            if (!ModelState.IsValid)
+                return RedirectToAction(nameof(Details), new { projectDetailsVM.Project.Id });
+
+            var result = _projectService.Update(projectDetailsVM);
+
+            if (result) TempData["success"] = "Project has been updated succesfully.";
+            else TempData["error"] = "Something went wrong while updating the project.";
+
+            return RedirectToAction(nameof(Details), new { projectDetailsVM.Project.Id });
+        }
+
+        public IActionResult Delete(int id)
+        {
+            var result = _projectService.Delete(id);
+
+            if (result) TempData["success"] = "Project has been deleted succesfully.";
+            else TempData["error"] = "Something went wrong while deleting the project.";
+
+            return RedirectToAction(nameof(Index));
+        }
+    }
+}
